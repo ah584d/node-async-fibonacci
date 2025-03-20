ba2293fb0fa296642c2225a2e958ba719f85c17c
Avraham Hamu <avraham.hamu@intl.att.com>
2025-03-20 16:22:23 +0200

    add fibonacci

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+function fibonacciAsync(limit: number): number {
+  if (limit <= 1) {
+    return 1;
+  }
+  return fibonacciAsync(limit - 1) + fibonacciAsync(limit - 2);
+}
+
+const result = fibonacciAsync(10);
+
+console.log(`Result is: ${result}`);
