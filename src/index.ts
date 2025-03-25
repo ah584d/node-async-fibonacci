9a38df8abc4ec619921afaab0a96afc61ddc259a
Avraham Hamu <avraham.hamu@intl.att.com>
2025-03-25 10:49:57 +0200

    update exercise

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-function fibonacciAsync(limit: number): number {
+const LIMIT_RUN = 45;
+
+async function longTask(limit: number): Promise<number> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const result = fibonacci(limit);
+      resolve(result);
+    }, 0);
+  });
+}
+
+function fibonacci(limit: number): number {
   if (limit <= 1) {
     return 1;
   }
-  return fibonacciAsync(limit - 1) + fibonacciAsync(limit - 2);
+  return fibonacci(limit - 1) + fibonacci(limit - 2);
 }
 
-const result = fibonacciAsync(10);
+async function main() {
+  console.log("Starting calculation...");
+  const result = await longTask(LIMIT_RUN);
+  console.log(`Result is: ${result}`);
+  console.log("continue calculation...");
+}
 
-console.log(`Result is: ${result}`);
+main();
